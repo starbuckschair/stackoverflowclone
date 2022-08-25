9fe11534dfc9829f00441f31755458c9f40be8d4
starbuckschair <water1793@gmail.com>
2022-08-25 11:10:33 +0900

    mainheader

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <>
       <header>
-        <h3>navbar menu1</h3>
+        <div className='logo'>로고</div>
+        <div className='product'>Products</div>
+        <input type="text" className="headerInput" placeholder="(돋보기아이콘)Search..."></input>
+        <div className='headerIcon'>myicon</div>
+        <div className='headerIcon'>icon1</div>
+        <div className='headerIcon'>icon1</div>
+        <div className='headerIcon'>icon1</div>
+        <div className='headerIcon'>icon1</div>
       </header>
       <aside>
         <h3>sidebar menu1</h3>
