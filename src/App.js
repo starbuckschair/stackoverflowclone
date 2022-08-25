1a9434e1facb023f4132d31ca181bf11dd55a7b2
starbuckschair <water1793@gmail.com>
2022-08-25 15:12:52 +0900

    article추가

--- src/App.js
+++ src/App.js
@@ -23,12 +23,56 @@ function App() {
         <p> Users</p>
         <p> Companies</p>
       </aside>
-      <section>
-        <article>
-          <div>content1</div>
-          <div>content2</div>
-        </article>
-      </section>
+      <article>
+        <section>
+          <div className="headline">All Question</div>
+          <div className="questionButton">
+          <button>Ask Question</button>
+          </div>
+        </section>
+        <section>
+          <div className="TotalQuestions">
+            <p>22,927,831 questions</p>
+          </div>
+          <div className="filterBar">
+            <div className="filterBarButton">Newest</div>
+            <div className="filterBarButton Active">Active</div>
+            <div className="filterBarButton Bountied">Bountied</div>
+            <div className="filterBarButton Unanswered">Unanswered</div>
+            <div className="filterBarButton">
+            
+  					<select id="uclass">
+  						<option value="archi">Frequent</option>
+  						<option value="mechanic">Score</option>
+  						<option value="indust">Unanswered</option>
+  						<option value="elec">전기전자공학과</option>
+  						<option value="computer" selected>More</option>
+  						<option value="chemical">Custom Filters</option>
+  					</select>
+            </div>
+            <div className="filterBarButton">filter</div>
+          </div>
+        </section>
+        <section>
+          <div className="filteredQuestion">
+            <div className="leftBox">
+              <div className="leftBoxSons">votes</div>
+              <div className="leftBoxSons">answers</div>
+              <div className="leftBoxSons">views</div>
+            </div>
+            <div className="rightBox">
+              <div className="rightBoxSons">volume sizing attached to EC2</div>
+              <div className="rightBoxSons2">Inside an EC2 I have docker with a container that I can't lose, so I noticed that I was out of space on the attached and exclusive volume for docker. So I increased it with another 15GB and execut</div>
+              <div className="rightBoxSons3">
+                <div className="tagBox">tag tag tag</div>
+                <div className="currentAskedTime">
+                user avatar David 264 asked 1 min ago
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </article>
       <footer>
         <div>footer내용 </div>
         {/* 주소입력 */}
