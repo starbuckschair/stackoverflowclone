2bce3c3f907fc43803b7042889c31f3f6774779d
Eunjoo Lee <isej0916@gmail.com>
2022-08-25 15:10:55 +0900

    footer/section추가

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ function App() {
   return (
     <>
       <header>
-        <div className='logo'>로고</div>
-        <div className='product'>Products</div>
-        <input type="text" className="headerInput" placeholder="(돋보기아이콘)Search..."></input>
-        <div className='headerIcon'>myicon</div>
-        <div className='headerIcon'>icon1</div>
-        <div className='headerIcon'>icon1</div>
-        <div className='headerIcon'>icon1</div>
-        <div className='headerIcon'>icon1</div>
+        <div className="logo">로고</div>
+        <div className="product">Products</div>
+        <input
+          type="text"
+          className="headerInput"
+          placeholder="(돋보기아이콘)Search..."
+        ></input>
+        <div className="headerIcon">myicon</div>
+        <div className="headerIcon">icon1</div>
+        <div className="headerIcon">icon1</div>
+        <div className="headerIcon">icon1</div>
+        <div className="headerIcon">icon1</div>
       </header>
       <aside>
         <h3>Home</h3>
@@ -25,12 +29,56 @@ function App() {
       </aside>
       <section>
         <article>
-          <div>content1</div>
-          <div>content2</div>
+          <div>
+            <div>content1</div>
+            <div>content2</div>
+          </div>
+          <div className="rightco">
+            <div className="yellowpost">
+              <h4>The Overflow Blog</h4>
+              <h4>Featured on Meta</h4>
+            </div>
+            <div className="seeall">
+              <h4>Collective</h4>
+              <p>
+                Google Cloud<button>join</button>
+              </p>
+
+              <p>
+                GitLab<button>join</button>
+              </p>
+
+              <p>
+                WSO2<button>join</button>
+              </p>
+            </div>
+          </div>
         </article>
       </section>
       <footer>
-        <div>footer내용 </div>
+        <div>logoicon</div>
+        <div>
+          <p>STACK OVERFLOW</p>
+          <h4>Question</h4>
+          <p>Help</p>
+        </div>
+        <div>
+          <h4>PRODUCT</h4>
+        </div>
+        <div>
+          <h4>COMPANY</h4>
+        </div>
+        <div>
+          <h4>STACK OVERFLOW NESTWORK</h4>
+        </div>
+        <div>
+          <p>Blog</p>
+          <p>Facebook</p>
+          <p>Twitter</p>
+          <p>LinkedIn</p>
+          <p>instagram</p>
+          <div>footer내용 </div>
+        </div>
         {/* 주소입력 */}
       </footer>
     </>
